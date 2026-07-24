65bb15ca73aee69cd97324465f6eb76fa7be18da
aryan <aryanagg04@gmail.com>
2026-07-24 12:07:45 +0530

    Added: Linting

--- src/app/app.py
+++ src/app/app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def home():
     return {"message": "Hello CI/CD!"}
 
+
 @app.get("/health")
 def health():
     return {"message": "Healthy"}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
